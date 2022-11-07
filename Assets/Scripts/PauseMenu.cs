40217ccef2096cf16e215220faf0d0d2a85a8a73
anakin-hub <anakin.wurdel@gmail.com>
2022-11-07 11:28:06 -0300

    creation of PausedMenu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPause;
+    [SerializeField] protected GameObject PauseMenuUI;
+
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("ESC!");
+            if(GameIsPause)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPause = false;
+    }
+    public void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPause = true;
+    }
+
+    public void Menu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MenuScene");
+    }
+
+    public void OpenInventory()
+    {
+        Debug.Log("inventario");
+    }
+
+    public void OpenQuests()
+    {
+        Debug.Log("Missões");
+    }
+
+
+}
